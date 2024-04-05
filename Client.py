bc7e07b9adab81b94061699e235d0eaff4a5f0de
Jaume Tello <jaume.tello@estudiants.urv.cat>
2024-04-05 13:36:02 +0200

    protos afegits

---
+++ Client.py
@@ -0,0 +1,1 @@
+import grpc, grpc_tools
---
+++ Main.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+def main():
+    try:
+        subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
+        print("Successfully installed requirements from requirements.txt")
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+        
+    #python3 -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. --pyi_out=. ./Client.proto
+    subprocess.check_call(['python3', '-m', 'grpc_tools.protoc', '-I./', '--python_out=.', '--grpc_python_out=.', '--pyi_out=.', './Client.proto'])
+    #python3 -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. --pyi_out=. ./MessageBroker.proto
+    subprocess.check_call(['python3', '-m', 'grpc_tools.protoc', '-I./', '--python_out=.', '--grpc_python_out=.', '--pyi_out=.', './MessageBroker.proto'])
+    #python3 -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. --pyi_out=. ./NameServer.proto
+    subprocess.check_call(['python3', '-m', 'grpc_tools.protoc', '-I./', '--python_out=.', '--grpc_python_out=.', '--pyi_out=.', './NameServer.proto'])
+
+main()
+    
